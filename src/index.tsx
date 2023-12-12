ed120d1ba1438e1d798a359f87ca961c2a7845fb
SquirrelWithAvocado <aalxalx@bk.ru>
2023-12-13 01:56:45 +0500

    fix-0.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/common/app/app';
-import { film } from './mocks/film';
-import { films } from './mocks/films';
-import { promo } from './mocks/promo';
-import { reviews } from './mocks/reviews';
-import { similar } from './mocks/similar';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { fetchFilmsAction, fetchPromoAction } from './store/api-actions';
